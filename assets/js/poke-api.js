bf6e4868b4ea1ebcdfd13fca66c0593a50d5f342
root <root@kalipc.internet.fazzy.net.br>
2026-05-17 22:28:51 -0300

    Versão 3.2.1 da Pokédex - Grande mudança implementação script gerar lista com pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,31 +1,14 @@
 
-
-// pokeApi.getPokemons = (offset = 0, limit = 5) => {
-//     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-//     return fetch(url)
-//         .then((response) => response.json())
-//         .then((jsonBody) => jsonBody.results)
-//         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-//         .then((detailRequests) => Promise.all(detailRequests))
-//         .then((pokemonsDetails) => pokemonsDetails)
-// }
+const pokeApi = {}
 
 
-// URL: https://pokeapi.co/api/v2/pokemon?type=grass&name=i
-// type = grass
-// name = 1
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?${offset}&limit=${limit}`;
+    return fetch(url) //estamos fazendo requisição http para converter
+        .then((response) =>  response.json())  // transforma string em json 
+        .then((jsonBody) => jsonBody.results)
+        .catch((error) => console.error(error))
+};
 
 
-//https://pokeapi.co/api/v2/pokemon?offset=
-// ${offset}&
-// limit=${limit}
 
-// Request Header
-//     content-type: application/jason
-
-
-// body 
-// {
-//     "name": "Teste"
-// }
