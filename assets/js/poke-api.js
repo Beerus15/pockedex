0ae25c1917489eb483e7e08d41a9cebcb2bf7ba0
root <root@kalipc.internet.fazzy.net.br>
2026-05-19 23:57:29 -0300

    Versão 5.1.1 da Pokédex - Mudança dos types colors e pequenas correção nas cores

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -29,7 +29,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         
 }
 
-pokeApi.getPokemons = (offset = 10, limit = 100) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?${offset}&limit=${limit}`;
     return fetch(url) //estamos fazendo requisição http para converter
         .then((response) =>  response.json())  // transforma string em json 
